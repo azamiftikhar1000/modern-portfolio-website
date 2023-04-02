2174fab03c4e0cea8da2a8c749dccb282a08f9c2
Azam Iftikhar <azamiftikhar1000@gmail.com>
2023-04-02 16:20:12 +0530

    project update

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -150,7 +150,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "34f999d7fdea4bf9"; }
+/******/ 		__webpack_require__.h = function() { return "a3604eefe054101e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
