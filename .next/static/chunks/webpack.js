f9adc52fdc4fc7251cfe5816387bbc68cb4d1c98
Azam Iftikhar <azamiftikhar1000@gmail.com>
2023-04-03 17:38:05 +0530

    site map changes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -54,6 +54,11 @@
 /******/ 	__webpack_require__.i = [];
 /******/ 	
 /************************************************************************/
+/******/ 	/* webpack/runtime/amd options */
+/******/ 	!function() {
+/******/ 		__webpack_require__.amdO = {};
+/******/ 	}();
+/******/ 	
 /******/ 	/* webpack/runtime/chunk loaded */
 /******/ 	!function() {
 /******/ 		var deferred = [];
@@ -150,7 +155,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "212b9117dcc64865"; }
+/******/ 		__webpack_require__.h = function() { return "68b6dae1a32d0538"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
